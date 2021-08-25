81b1c8976d67dcb89ba6743d188b5206ad8fd14c
trinh <trinh.dang@codestringers.com>
2021-08-25 13:15:59 +0700

    3.2 - express

--- server.js
+++ server.js
@@ -1,1 +1,27 @@
-console.log("server setup");
+import express from "express";
+import cors from "cors";
+const morgan = require("morgan");
+require("dotenv").config();
+
+
+// create express app
+const app = express();
+
+// apply middleware
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+app.use((req, res, next) => {
+    console.log("this is my own middleware");
+    next();
+});
+
+// route
+app.get('/', (req, res) => {
+    res.send('you hit server endpoint');
+});
+
+// port
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
