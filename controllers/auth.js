82af7d6b2a3eaddc832a66f057b90c28e7a31c98
trinh <trinh.dang@codestringers.com>
2021-08-25 17:10:01 +0700

    4 - registration

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,33 @@
+import User from "../models/user";
+import { hashPassword } from "../utils/auth";
 
+export const register = async (req, res) => {
+    try {
+        // console.log(req.body);
+        const {name, email, password} = req.body;
 
-export const register = (req, res) => {
-    console.log(req.body);
-    res.json('register user response from controller');
+        // validation
+        if(!name) return res.status(400).send("Name is required.");
+        if(!password || password.length < 6) {
+            return res.status(400).send("Password is required and should contain at least 6 characters");
+        }
+        let userExist = await User.findOne({email}).exec();
+        if(userExist) return res.status(400).send("Email is taken.");
+
+        // hash password
+        const hashedPassword = await hashPassword(password);
+
+        // register
+        const user = new User({
+            name,
+            email,
+            password: hashedPassword,
+        });
+        await user.save();
+        console.log("saved user: ", user);
+        return res.json({ok: true});
+    } catch (err) {
+        console.log(err);
+        return res.status(400).send("Error. Try again.");
+    }
 }
